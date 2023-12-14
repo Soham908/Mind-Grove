7faac6502b067abbf1a938ace835860ed1c829e6
Love jain <love.j@somaiya.edu>
2023-12-14 11:49:54 +0530

    Counsellor added

--- src/pages/Counsellor.js
+++ src/pages/Counsellor.js
@@ -1,8 +1,14 @@
+import "./Counsellor.css"
+import Doctor from "../images/doc3.png"
 
 export const Counsellor = () => {
     return (
-        <div>
-            counsellor page
+        <div className="Counsel">
+            <div className="Counsel-card">
+            <img src={Doctor} class="card-img-top" alt="..." />
+                <p className="txt" > Dr. Emily Thompson</p>
+                <p className="txt">Details</p>
+            </div>
         </div>
     )
 }
